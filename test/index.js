43ae5101e25ce7fa88edd74a24114327a6853a98
Eugene Ware <eugene@noblesamurai.com>
2014-02-16 14:41:03 +1100

    postgres and mysql working

--- test/index.js
+++ test/index.js
@@ -3,16 +3,11 @@ var expect = require('expect.js'),
     spawn = require('..');
 
 describe('docker-spawn', function() {
-  var dockerhost = 'docker';
+  var dockerhost = process.env.DOCKER_HOST || 'docker';
+
   it('should be able to spin up a mysql server', function(done) {
     this.timeout(0);
 
-    /*
-    var opts = {
-      image: 'orchardup/postgresql',
-      ports: [5432]
-    };
-    */
     var opts = {
       image: 'orchardup/mysql',
       ports: [3306]
@@ -20,6 +15,7 @@ describe('docker-spawn', function() {
 
     spawn(dockerhost, opts, work);
     function work(err, exposed, stop) {
+      if (err) return done(err);
       sequelize = new Sequelize('mysql', 'root', '', {
         dialect: 'mysql',
            host: dockerhost,
@@ -31,15 +27,69 @@ describe('docker-spawn', function() {
       sequelize
         .authenticate()
         .done(function(err) {
-          if (!!err) {
+          if (err) {
             console.log('Unable to connect to the database:', err)
             stop(done);
           } else {
             console.log('Connection has been established successfully.')
             sequelize.query('select * from user').done(function (err, data) {
               if (err) throw err;
-              console.log(data.length);
-              console.log(data);
+              expect(data.length).to.equal(8);
+              stop(done);
+            });
+          }
+        });
+    }
+  });
+
+  it('should be able to spin up a postgresql server', function(done) {
+    this.timeout(0);
+
+    var opts = {
+      image: 'orchardup/postgresql',
+      ports: [5432]
+    };
+
+    spawn(dockerhost, opts, work);
+    function work(err, exposed, stop) {
+      if (err) return done(err);
+      sequelize = new Sequelize('docker', 'docker', 'docker', {
+        dialect: 'postgres',
+           host: dockerhost,
+           port: exposed[0],
+           pool: {
+             handleDisconnects: true
+           }
+      });
+      sequelize
+        .authenticate()
+        .done(function(err) {
+          if (err) {
+            console.log('Unable to connect to the database:', err)
+            stop(done);
+          } else {
+            console.log('Connection has been established successfully.')
+            sequelize.query('select * from pg_catalog.pg_user').done(function (err, data) {
+              if (err) throw err;
+              expect(data).to.eql(
+                [ { usename: 'postgres',
+                    usesysid: 10,
+                    usecreatedb: true,
+                    usesuper: true,
+                    usecatupd: true,
+                    userepl: true,
+                    passwd: '********',
+                    valuntil: null,
+                    useconfig: null },
+                  { usename: 'docker',
+                    usesysid: 11920,
+                    usecreatedb: false,
+                    usesuper: true,
+                    usecatupd: true,
+                    userepl: true,
+                    passwd: '********',
+                    valuntil: null,
+                    useconfig: null } ]);
               stop(done);
             });
           }
